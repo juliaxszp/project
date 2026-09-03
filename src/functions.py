cb4779cce84a72aa2589e6a85acb81b74e71a03f
Paliciak <246253269+Paliciak@users.noreply.github.com>
2026-09-03 14:32:48 +0200

    pierwsza czesc, jutro dokoncze

--- src/functions.py
+++ src/functions.py
@@ -1,1 +1,13 @@
 from basics import *
+from pysat.solvers import Kissat404
+
+def permutation(builder, input_vars, perm):
+    output_vars = []
+
+    for i in range(len(perm)):
+        input_var = input_vars[perm[i]]
+        output_var = builder.var(f"out_{i}")
+        builder.equals(output_var, input_var)
+        output_vars.append(output_var)
+
+    return output_vars
--- src/main.py
+++ src/main.py
@@ -1,12 +1,50 @@
-from basics import *
+from basics import BasicFunctions
+from functions import permutation
+from pysat.solvers import Kissat404
 
 builder = BasicFunctions()
 
-x = builder.var("x")
-y = builder.var("y")
-z = builder.var("z")
-w = builder.var("w")
+a = builder.var("a")
+b = builder.var("b")
+c = builder.var("c")
+d = builder.var("d")
 
-builder.equal_or(x, [y, z])
+output = permutation(
+    builder,
+    [a, b, c, d],
+    [2, 0, 3, 1]
+)
 
-print(builder.cnf.clauses)
+print("Zmienne wejściowe:", [a, b, c, d])
+print("Zmienne wyjściowe:", output)
+
+builder.cnf.append([a])
+builder.cnf.append([-b])
+builder.cnf.append([-c])
+builder.cnf.append([d])
+
+with Kissat404(bootstrap_with=builder.cnf.clauses) as solver:
+
+    if solver.solve():
+        model = solver.get_model()
+
+        print("Model:", model)
+
+        output_values = []
+
+        for var in output:
+            if var in model:
+                output_values.append(1)
+            else:
+                output_values.append(0)
+
+        print("Wejście:     ", [1, 0, 0, 1])
+        print("Permutacja:  ", [2, 0, 3, 1])
+        print("Wyjście:     ", output_values)
+
+        assert output_values == [0, 1, 1, 0]
+
+        print("PERMUTACJA DZIAŁA POPRAWNIE")
+
+    else:
+        print("UNSAT - coś jest nie tak")
