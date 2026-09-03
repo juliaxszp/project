8af6d24a77432b1419e84d9cd71b468d4f56a6ae
juliaxszp <szpiglerj@gmail.com>
2026-09-03 13:40:06 +0200

    dodana pierwsza funkckja do permutacji photon beetle

--- src/functions.py
+++ src/functions.py
@@ -1,1 +1,19 @@
 from basics import *
+
+#const
+RC = [1, 3, 7, 14, 13, 11, 6, 12, 9, 2, 5, 10]
+IC = [0, 1, 3, 7, 15, 14, 12, 8]
+
+
+#czesc do addconstant
+def xor_bits(self, x, y, b):   #b = y xor x, gdzie y to nowy bit
+    if b == 0:  # b = 0, czyli y = x
+        self.xor([x, y])
+    else:  # b = 1, czyli y = not x
+        self.xor([x, -y])
+
+def xor_const(self, var_1, var_2, var_3):
+    for i in range(len(var_1)):                              #zmiana z 4 bitów na dlugosc jednej zmiennej na potrzeby innych szyfrow
+        self.xor_bits(var_1[i], var_2[i], var_3[i])
+
+
--- src/photon.py
+++ src/photon.py
@@ -1,4 +1,12 @@
 from functions import *
 
-RC = [1, 3, 7, 14, 13, 11, 6, 12, 9, 2, 5, 10]
-IC = [0, 1, 3, 7, 15, 14, 12, 8]
+def addconstantphoton(self, state, new_state, k):
+    for i in range(8):
+        constant = RC[k] ^ IC[i]
+        constant_bits = [(constant >> j) & 1 for j in range(4)]
+        self.xor_const(state[i][0], new_state[i][0], constant_bits)
+    for j in range(1, 8):
+        for i in range(8):
+            self.equals(state[i][j], new_state[i][j])
+
+
