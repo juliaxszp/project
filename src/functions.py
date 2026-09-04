3c7fa890fd8d530d89998bd51cb963e1e8b15ee7
juliaxszp <szpiglerj@gmail.com>
2026-09-04 09:41:52 +0200

    dodana funkcja do photona

--- src/functions.py
+++ src/functions.py
@@ -3,6 +3,8 @@ from basics import *
 #const
 RC = [1, 3, 7, 14, 13, 11, 6, 12, 9, 2, 5, 10]
 IC = [0, 1, 3, 7, 15, 14, 12, 8]
+SBOXphoton = [0xC, 0x5, 0x6, 0xB, 0x9, 0x0, 0xA, 0xD, 0x3, 0xE, 0xF, 0x8, 0x4, 0x7, 0x1, 0x2]
+
 
 #czesc do addconstant
 def xor_bits(self, x, y, b):   #b = y xor x, gdzie y to nowy bit
@@ -14,3 +16,5 @@ def xor_bits(self, x, y, b):   #b = y xor x, gdzie y to nowy bit
 def xor_const(self, var_1, var_2, var_3):
     for i in range(len(var_1)):                              #zmiana z 4 bitów na dlugosc jednej zmiennej na potrzeby innych szyfrow
         self.xor_bits(var_1[i], var_2[i], var_3[i])
+
+
