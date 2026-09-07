18b68c500a45eda7a6521fdf97520c75614c6c89
root <root@laptop-Adama.localdomain>
2026-09-07 14:05:02 +0200

    rozpoczecie tworzenia podstawowych funkcji potrzebnych do xoodoo

---
+++ src/xoodiak_v1.py
@@ -0,0 +1,12 @@
+#Adam Aftyka implementacja xoodyak
+#stale ustalony klucz:
+#stale ustalony Nonce:
+#stale ustalone AD:
+from .functions import *
+
+tablica = []
+def xoodoo(tablica):
+    for itterator in range(12):
+        
+        #theta:
+        
