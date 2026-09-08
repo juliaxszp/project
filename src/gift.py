6d01ec67c346f886f1b828d882c66aad27cfec8d
Paliciak <246253269+Paliciak@users.noreply.github.com>
2026-09-08 12:47:25 +0200

    Permutacja z testami do gift

--- src/gift.py
+++ src/gift.py
@@ -1,6 +1,5 @@
 from .functions import sbox
 
-
 GIFT_SBOX = [
     0x1, 0xA, 0x4, 0xC,
     0x6, 0xF, 0x3, 0x9,
@@ -8,6 +7,32 @@ GIFT_SBOX = [
     0x5, 0x0, 0x8, 0xE
 ]
 
+GIFT_PERM = [
+    [
+        0, 4, 8, 12, 16, 20, 24, 28,
+        3, 7, 11, 15, 19, 23, 27, 31,
+        2, 6, 10, 14, 18, 22, 26, 30,
+        1, 5, 9, 13, 17, 21, 25, 29
+    ],
+    [
+        1, 5, 9, 13, 17, 21, 25, 29,
+        0, 4, 8, 12, 16, 20, 24, 28,
+        3, 7, 11, 15, 19, 23, 27, 31,
+        2, 6, 10, 14, 18, 22, 26, 30
+    ],
+    [
+        2, 6, 10, 14, 18, 22, 26, 30,
+        1, 5, 9, 13, 17, 21, 25, 29,
+        0, 4, 8, 12, 16, 20, 24, 28,
+        3, 7, 11, 15, 19, 23, 27, 31
+    ],
+    [
+        3, 7, 11, 15, 19, 23, 27, 31,
+        2, 6, 10, 14, 18, 22, 26, 30,
+        1, 5, 9, 13, 17, 21, 25, 29,
+        0, 4, 8, 12, 16, 20, 24, 28
+    ]
+]
 
 def gift_subcells(builder, state, prefix="gift_subcells"):
     output_state = [
@@ -37,5 +62,30 @@ def gift_subcells(builder, state, prefix="gift_subcells"):
         output_state[1].append(output_vars[1])
         output_state[2].append(output_vars[2])
         output_state[3].append(output_vars[3])
+
+    return output_state
+
+def gift_permbits(builder, state, prefix="gift_permbits"):
+    output_state = [
+        [],
+        [],
+        [],
+        []
+    ]
+
+    for row in range(4):
+        for i in range(32):
+            output_state[row].append(
+                builder.var(f"{prefix}_s{row}_{i}")
+            )
+
+    for row in range(4):
+        for source in range(32):
+            destination = GIFT_PERM[row][source]
+
+            builder.equals(
+                output_state[row][destination],
+                state[row][source]
+            )
 
     return output_state
--- tests/test_gift.py
+++ tests/test_gift.py
@@ -1,9 +1,83 @@
 from src.basics import BasicFunctions
-from src.gift import gift_subcells, GIFT_SBOX
+from src.gift import gift_subcells, GIFT_SBOX, gift_permbits, GIFT_PERM
 from pysat.solvers import Kissat404
 
 def test_gift_subcells():
-    for input_value in range(16):
+    builder = BasicFunctions()
+
+    state = [
+        [],
+        [],
+        [],
+        []
+    ]
+
+    for row in range(4):
+        for i in range(32):
+            state[row].append(
+                builder.var(f"state_{row}_{i}")
+            )
+
+    output_state = gift_subcells(
+        builder,
+        state,
+        "test_subcells"
+    )
+
+    input_values = []
+
+    for i in range(32):
+        input_values.append(i % 16)
+
+    for column in range(32):
+        input_value = input_values[column]
+
+        input_bits = []
+
+        for i in range(4):
+            bit = (input_value >> i) & 1
+            input_bits.append(bit)
+
+        for row in range(4):
+            var = state[row][column]
+            bit = input_bits[row]
+
+            if bit == 1:
+                builder.cnf.append([var])
+            else:
+                builder.cnf.append([-var])
+
+    with Kissat404(
+        bootstrap_with=builder.cnf.clauses
+    ) as solver:
+
+        assert solver.solve()
+
+        model = solver.get_model()
+
+    for column in range(32):
+        output_bits = []
+
+        for row in range(4):
+            var = output_state[row][column]
+
+            if var in model:
+                output_bits.append(1)
+            else:
+                output_bits.append(0)
+
+        output_value = 0
+
+        for i, bit in enumerate(output_bits):
+            output_value |= bit << i
+
+        input_value = input_values[column]
+        expected_output = GIFT_SBOX[input_value]
+
+        assert output_value == expected_output
+
+def test_gift_permbits():
+    for source in range(32):
         builder = BasicFunctions()
 
         state = [
@@ -19,48 +93,35 @@ def test_gift_subcells():
                     builder.var(f"state_{row}_{i}")
                 )
 
-        output_state = gift_subcells(
+        output_state = gift_permbits(
             builder,
             state,
-            "test_subcells"
+            f"test_permbits_{source}"
         )
 
-        input_bits = []
-
-        for i in range(4):
-            bit = (input_value >> i) & 1
-            input_bits.append(bit)
-
         for row in range(4):
-            var = state[row][0]
-            bit = input_bits[row]
+            for i in range(32):
+                var = state[row][i]
 
-            if bit == 1:
-                builder.cnf.append([var])
-            else:
-                builder.cnf.append([-var])
+                if i == source:
+                    builder.cnf.append([var])
+                else:
+                    builder.cnf.append([-var])
 
         with Kissat404(
             bootstrap_with=builder.cnf.clauses
         ) as solver:
 
             assert solver.solve()
-
             model = solver.get_model()
 
-        output_bits = []
-
         for row in range(4):
-            var = output_state[row][0]
+            expected_destination = GIFT_PERM[row][source]
 
-            if var in model:
-                output_bits.append(1)
-            else:
-                output_bits.append(0)
+            for destination in range(32):
+                var = output_state[row][destination]
 
-        output_value = 0
-
-        for i, bit in enumerate(output_bits):
-            output_value |= bit << i
-
-        assert output_value == GIFT_SBOX[input_value]
+                if destination == expected_destination:
+                    assert var in model
+                else:
+                    assert -var in model
