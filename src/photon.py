f7623d709cb866c4f0d8487813ee0e87b6ed3aef
juliaxszp <szpiglerj@gmail.com>
2026-09-09 09:54:03 +0200

    pełna implementacja permutacji photon z testami

--- src/photon.py
+++ src/photon.py
@@ -44,20 +44,20 @@ def sbox_photon(Builder, state, new_state):
                     Builder.cnf.append(clause)
             
 
-def mix_columns_photon(Builder, state, new_state):
+def mix_columns_photon(Builder, state, new_state, prefix):
 
     for l in range(8):
         column = [state[j][l] for j in range(8)]
 
         for step in range(8):
-            products = gf16_const_mult(Builder, column, f"column_{l}, step_{step}")
+            products = gf16_const_mult(Builder, column, f"{prefix}_column_{l}, step_{step}")
 
             next_column = []
 
             for i in range(7):
                 nibble = []
                 for bit in range(4):
-                    x = Builder.var(f"column{l}_step_{step}_nibble_{i}_{bit}")
+                    x = Builder.var(f"{prefix}_column{l}_step_{step}_nibble_{i}_{bit}")
                     Builder.equals(x, column[i+1][bit])
                     nibble.append(x)
                 next_column.append(nibble)
@@ -65,7 +65,7 @@ def mix_columns_photon(Builder, state, new_state):
             last_nibble = []
 
             for bit in range(4):
-                x = Builder.var(f"column_{l}_step{step}_last_{bit}")
+                x = Builder.var(f"{prefix}_column_{l}_step{step}_last_{bit}")
                 Builder.xor([x] + [product[bit] for product in products])
                 last_nibble.append(x)
             next_column.append(last_nibble)
@@ -77,4 +77,34 @@ def mix_columns_photon(Builder, state, new_state):
                 Builder.equals(new_state[i][l][bit], column[i][bit])
 
 
+def create_state(Builder, name):
+    state = []
+
+    for i in range(8):
+        row = []
+        for j in range(8):
+            nibble = []
+            for b in range(4):
+                nibble.append(Builder.var(f"{name}_{i}_{j}_{b}"))
+            row.append(nibble)
+        state.append(row)
+    return state
+
+def photon_permutation(Builder, state):
+    current_state = state
+
+    for round in range(12):
+        state_after_constant = create_state(Builder, f"round_{round}_constant")
+        state_after_sbox = create_state(Builder,  f"round_{round}_sbox")
+        state_after_shiftrows = create_state(Builder,  f"round_{round}_shiftrows")
+        state_after_mixcolumns = create_state(Builder,  f"round_{round}_mixcolumns")
+
+        add_constant_photon(Builder, current_state, state_after_constant, round)
+        sbox_photon(Builder, state_after_constant, state_after_sbox)
+        shift_rows_photon(Builder, state_after_sbox, state_after_shiftrows)
+        mix_columns_photon(Builder, state_after_shiftrows, state_after_mixcolumns, f"round_{round}")
+
+        current_state = state_after_mixcolumns
+
+    return current_state
 
