ae1257d28e02151b68daf6b85ecc45b191a02b82
MuraszkoM <u58785@student.wat.edu.pl>
2026-09-10 10:35:08 +0200

    zmiana

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -312,7 +312,7 @@ def test_rotate_right():
          [14, 15, 16, 17, 18, 19, 20, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]),
 
          (list(range(64)), 13,
-         list(range(51, 64)) + list(range(51)))
+         list(range(51, 64)) + list(range(51))),
 
     ]
 
